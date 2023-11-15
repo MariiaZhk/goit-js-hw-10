1797dd3d8b56c987c3d10fb5ba1f13e39b54c952
MariiaZhk <maria8zhuk@gmail.com>
2023-11-15 18:56:12 +0100

    slim

--- src/index.js
+++ src/index.js
@@ -40,11 +40,11 @@ function onSelectBreedElChange(event) {
 
   fetchCatByBreed(breedId)
     .then(breedsIdArr => {
-      refs.loaderEl.classList.replace('loader', 'is-hidden');
       const { url, breeds } = breedsIdArr[0];
 
       refs.containerCatInfoEl.innerHTML = `<div class="img-container"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="description-container"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
 
+      refs.loaderEl.classList.replace('loader', 'is-hidden');
       refs.selectBreedEl.classList.remove('is-hidden');
       refs.containerCatInfoEl.classList.remove('is-hidden');
     })
